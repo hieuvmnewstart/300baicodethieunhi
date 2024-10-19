5003039e99f9483e9bb11763586c156dd4f3df79
hieuvmnewstart <hieuvm13newstart@gmail.com>
2024-10-19 18:09:23 +0700

    Update Find the Runner-Up Score!.py

--- Find the Runner-Up Score!.py
+++ Find the Runner-Up Score!.py
@@ -1,11 +1,25 @@
-Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. You are given  scores. Store them in a list and find the score of the runner-up.
+# Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. You are given  scores. Store them in a list and find the score of the runner-up.
 
-Input Format
+# Input Format
 
-The first line contains . The second line contains an array   of  integers each separated by a space.
+# The first line contains . The second line contains an array   of  integers each separated by a space.
 
-Constraints
+# Constraints
 
-Output Format
+# Output Format
 
-Print the runner-up score.
+# Print the runner-up score.
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    list1 = list(arr)
+    list1.sort(reverse = True)
+    max_ = list1[0]
+    for i in list1:
+        if i == max_:
+            continue
+        else:
+            print(i)
+            break
+    
