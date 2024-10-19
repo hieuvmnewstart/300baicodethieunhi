3a51913f6a3147e4e2cd6379d6830a8894756be6
hieuvmnewstart <hieuvm13newstart@gmail.com>
2024-10-19 17:21:37 +0700

    Update List Comprehensions.py

--- List Comprehensions.py
+++ List Comprehensions.py
@@ -1,23 +1,30 @@
-Let's learn about list comprehensions! You are given three integers  and  representing the dimensions of a cuboid along with an integer . Print a list of all possible coordinates given by  on a 3D grid where the sum of  is not equal to . Here, . Please use list comprehensions rather than multiple loops, as a learning exercise.
+# Let's learn about list comprehensions! You are given three integers  and  representing the dimensions of a cuboid along with an integer . Print a list of all possible coordinates given by  on a 3D grid where the sum of  is not equal to . Here, . Please use list comprehensions rather than multiple loops, as a learning exercise.
 
-Example
+# Example
 
+# All permutations of  are:
+# .
 
-
-
-All permutations of  are:
-.
-
-Print an array of the elements that do not sum to .
+# Print an array of the elements that do not sum to .
 
 
-Input Format
+# Input Format
 
-Four integers  and , each on a separate line.
+# Four integers  and , each on a separate line.
 
-Constraints
+# Constraints
 
-Print the list in lexicographic increasing order.
+# Print the list in lexicographic increasing order.
 
-Sample Input 0
-
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    result=[]
+    for i in range(x+1):
+        for j in range (y+1):
+            for k in range (z+1):
+                if i+j+k !=n:
+                    result.append([i,j,k])
+    print(result)
